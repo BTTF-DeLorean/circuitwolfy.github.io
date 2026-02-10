e0de4251e0f431ebb9910180b9b39cea9d60acc0
BTTF-DeLorean <huyvu858@gmail.com>
2026-02-09 23:23:14 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// interactable buttons
+document.querySelectorAll(".skeuo-button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("Yooo, I didn't make this button do anything yet!");
+    });
+});
