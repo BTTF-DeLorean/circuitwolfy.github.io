4da43f2e3def052a8e3394d4b11141fdb2f3c16b
Circuit Wolfy <huyvu858@gmail.com>
2026-09-05 19:38:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ document.querySelectorAll(".skeuo-button").forEach(btn => {
         alert("Yooo, I didn't make this button do anything yet!");
     });
 });
+
+function openLightbox(img) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-image");
+    
+    lightbox.classList.add("active");
+    lightboxImg.src = img.src;
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById("lightbox");
+    lightbox.classList.remove("active");
+}
